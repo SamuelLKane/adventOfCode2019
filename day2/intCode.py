2e81d83bb0e7d6e46dc3ea9ab5d723e835c95cf2
Samuel Kane <samuel.l.kane@gmail.com>
2019-12-03 16:01:04 -0500

    feat: solution to day 2 - puzzle 2
    This one was pretty easy to extend. It's a brute force solution

--- day2/intCode.py
+++ day2/intCode.py
@@ -1,7 +1,6 @@
-pwd = "/Users/samuelkane/Documents/Development/adventOfCode2019/day2/"
-with open(pwd + 'input.data') as fh:
-    program = [int(value) for value in fh.read().split(',')]
-    print(program)
+def runProgram(index1Input, index2Input, program):
+    program[1] = index1Input
+    program[2] = index2Input
     for i in range(0, len(program), 4):
         opcode = program[i]
         if opcode == 99:
@@ -13,4 +12,15 @@ with open(pwd + 'input.data') as fh:
             program[dest] = program[index1] * program[index2]
         else:
             raise Exception(f"Invalid opcode {opcode}")
+    return(program[0])
+
+
+pwd = "/Users/samuelkane/Documents/Development/adventOfCode2019/day2/"
+with open(pwd + 'input.data') as fh:
+    program = [int(value) for value in fh.read().split(',')]
     print(program)
+    for i1 in range(100):
+        for i2 in range(100):
+            result = runProgram(i1, i2, program[:])
+            if result == 19690720:
+                print(i1, i2)
