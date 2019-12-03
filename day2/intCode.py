e1c361074d0dac57af9ebceabfc3c9a81b04229b
Samuel Kane <samuel.l.kane@gmail.com>
2019-12-03 11:41:57 -0500

    feat: working day2 puzzle 1
    I'll need to pour over why this version works but my prior one isn't.
    Borrowed the idea of the range with step from reddit user __Mekakucity
    but otherwise used the same backbone as the prior implementation...

--- day2/intCode.py
+++ day2/intCode.py
@@ -1,26 +1,16 @@
-testInput = [2,3,0,3,99]
 pwd = "/Users/samuelkane/Documents/Development/adventOfCode2019/day2/"
 with open(pwd + 'input.data') as fh:
     program = [int(value) for value in fh.read().split(',')]
-    # program = testInput
-    currentBufferIndex = 0
-    while(True):
-        buffers = [program[i*4:(i+1) * 4] for i in range((len(program) + 3) // 4)]
-        print(buffers)
-        currentBuffer = buffers[currentBufferIndex]
-        if 99 in currentBuffer:
-            break  # reached a halt
-        addMult = currentBuffer[0]
-        index1 = currentBuffer[1]
-        index2 = currentBuffer[2]
-        dest = currentBuffer[3]
-        if addMult == 1:
-            print(f"{program[index1]} + {program[index2]} into {dest}")
+    print(program)
+    for i in range(0, len(program), 4):
+        opcode = program[i]
+        if opcode == 99:
+            break
+        index1, index2, dest = program[i+1], program[i+2], program[i+3]
+        if opcode == 1:
             program[dest] = program[index1] + program[index2]
-        elif addMult == 2:
-            print(f"{program[index1]} * {program[index2]} into {dest}")
+        elif opcode == 2:
             program[dest] = program[index1] * program[index2]
         else:
-            raise Exception(f"Invalid addMult value {addMult} in buffer {currentBuffer}".rstrip())
-        currentBufferIndex = currentBufferIndex + 1
+            raise Exception(f"Invalid opcode {opcode}")
     print(program)
